e804704c333119a7b137b143d725acae9e191b97
Ekmacmini32 <ekmacmini32@Ekmacmini32s-Mac-mini.local>
2025-10-10 18:00:37 +0545

    feat(api): Api ongoing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
                         </svg>
                     </button>
-                    <img src="{{ asset('images/logo.png') }}" alt="Logo" class="navbar-brand h1 mb-0 fw-bold text-dark !w-[10%]">
+                    <img src="{{ asset('images/logo.png') }}" alt="Logo" class="navbar-brand h1 mb-0 fw-bold text-dark !w-[10%]" style="object-fit: contain; width: 15%;">
                 </div>
                 <div class="navbar-nav ms-auto">
                     <span class="navbar-text text-dark">{{ date('M d, Y') }}</span>
